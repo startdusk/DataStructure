a5cdb8e694a376bf2edf0de5275f0cbd3f274c77
startdusk <522383345@qq.com>
2021-12-24 23:42:03 +0800

    add(array): add loop array

---
+++ Array/LoopArray.cs
@@ -0,0 +1,80 @@
+namespace DataStructure.Array;
+
+// 循环数组
+public class LoopArray<E>
+{
+    private E[] data;
+    private int first;
+    private int last;
+    private int N;
+
+    public LoopArray(int capacity)
+    {
+        data = new E[capacity];
+        first = 0;
+        last = 0;
+        N = 0;
+    }
+
+    public LoopArray() : this(10)
+    {
+
+    }
+
+    public int Count { get { return N; } }
+
+    public bool IsEmpty { get { return N == 0; } }
+
+
+    public void AddLast(E e)
+    {
+        if (N == data.Length)
+        {
+            ResetCapacity(data.Length * 2);
+        }
+        data[last] = e;
+        last = (last + 1) % data.Length;
+        N++;
+    }
+
+    public E RemoveFirst()
+    {
+        if (IsEmpty)
+        {
+            throw new InvalidOperationException("数组元素为空");
+        }
+
+        if (N == data.Length / 4)
+        {
+            ResetCapacity(data.Length / 2);
+        }
+
+        var ret = data[first];
+        data[first] = default(E);
+        first = (first + 1) % data.Length;
+        N--;
+        return ret;
+    }
+
+    public E GetFirst()
+    {
+        if (IsEmpty)
+        {
+            throw new InvalidOperationException("数组元素为空");
+        }
+
+        return data[first];
+    }
+
+    private void ResetCapacity(int newCapacity)
+    {
+        var newData = new E[newCapacity];
+        for (int i = 0; i < newCapacity; i++)
+        {
+            newData[i] = data[(first + 1) % data.Length];
+        }
+        data = newData;
+        first = 0;
+        last = N;
+    }
+}
