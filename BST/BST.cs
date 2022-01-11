e2d139b2ea1621fc2c7e2059c6e7443debc17817
startdusk <522383345@qq.com>
2022-01-11 22:36:37 +0800

    add(BST): add add method with no recursion

--- BST/BST.cs
+++ BST/BST.cs
@@ -28,4 +28,50 @@ public class BST<E> where E : IComparable<E>
     public int Count { get { return N; } }
 
     public bool IsEmpty { get { return N == 0; } }
+
+    // 非递归方式添加node
+    public void add(E e)
+    {
+        if (root == null)
+        {
+            root = new Node(e);
+            N++;
+            return;
+        }
+        Node pre = null;
+        Node cur = root;
+
+        // 循环遍历，找到该node应该放到那个父亲的节点下
+        while (cur != null)
+        {
+            // 节点已存在，跳过
+            if (e.CompareTo(cur.e) == 0)
+            {
+                return;
+            }
+
+            pre = cur;
+            if (e.CompareTo(cur.e) < 0)
+            {
+                // 左孩子
+                cur = cur.left;
+            }
+            else
+            {
+                // 右孩子
+                cur = cur.right;
+            }
+        }
+        cur = new Node(e);
+        if (e.CompareTo(cur.e) < 0)
+        {
+            pre.left = cur;
+        }
+        else
+        {
+            pre.right = cur;
+        }
+
+        N++;
+    }
 }
